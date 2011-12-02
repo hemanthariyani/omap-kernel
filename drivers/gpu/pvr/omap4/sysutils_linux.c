dedebe5a35bdb6b0dc40d8a540d74cc9d02c838e
Hemant Hariyani <hemanthariyani@ti.com>
2011-12-02 17:12:00 -0600

    Revert "gpu: pvr: implement intra-frame dvfs"
    This reverts commit 442799f7eb0dea1394d24288a8ec3f4bc8024f83.

--- drivers/gpu/pvr/omap4/sysutils_linux.c
+++ drivers/gpu/pvr/omap4/sysutils_linux.c
@@ -61,10 +61,6 @@
 #define SGX_PARENT_CLOCK "core_ck"
 #endif
 
-extern bool sgx_idle_logging;
-extern uint sgx_idle_mode;
-extern uint sgx_idle_timeout;
-
 #if defined(LDM_PLATFORM) && !defined(PVR_DRI_DRM_NOT_PCI)
 extern struct platform_device *gpsPVRLDMDev;
 #endif
@@ -195,8 +191,7 @@ PVRSRV_ERROR EnableSGXClocks(SYS_DATA *psSysData)
 	{
 		int res;
 
-		if (sgx_idle_mode == 0)
-			RequestSGXFreq(psSysData, IMG_TRUE);
+		RequestSGXFreq(psSysData, IMG_TRUE);
 
 		res = pm_runtime_get_sync(&gpsPVRLDMDev->dev);
 		if (res < 0)
@@ -248,8 +243,7 @@ IMG_VOID DisableSGXClocks(SYS_DATA *psSysData)
 			PVR_DPF((PVR_DBG_ERROR, "DisableSGXClocks: pm_runtime_put_sync failed (%d)", -res));
 		}
 
-		if (sgx_idle_mode == 0)
-			RequestSGXFreq(psSysData, IMG_FALSE);
+		RequestSGXFreq(psSysData, IMG_FALSE);
 	}
 #endif
 
